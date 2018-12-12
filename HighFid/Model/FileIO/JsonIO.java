7bc2c1702b759762cb4a27e0f28a475377ef0c9f
Tim <mesotten.tim@gmail.com>
2018-12-12 14:46:19 +0100

    Added structure for saving and loading Model state

--- HighFid/Model/FileIO/JsonIO.java
+++ HighFid/Model/FileIO/JsonIO.java
@@ -1,4 +1,84 @@
 package HighFid.Model.FileIO;
 
+//Personal Imports
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+//Java Imports
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * The JsonIO for the Model.
+ * This class allows saving and loading of JSON files.
+ *
+ * @author Tim Mesotten
+ */
 public class JsonIO {
+
+    //Public methods
+    /**
+     * Public function saveJSONFile
+     * Saves to a .json-file
+     *
+     * @param fileName          the directory of the file
+     * @param JSONModelObject   the object that has to be saved
+     * @throws Exception        when something goes wrong with loading
+     */
+    public static void saveJSONFile(String fileName, JSONObject JSONModelObject) throws Exception {
+        //Check extension
+        int j = fileName.lastIndexOf('.');
+        if (j == -1) {
+            fileName = fileName + ".json";
+        } else {
+            fileName = fileName.substring(0, j) + ".json";
+        }
+
+        //Write to file
+        try {
+            FileWriter file = new FileWriter(fileName);
+            file.write(JSONModelObject.toJSONString());
+            file.close();
+        } catch (IOException e) {
+            //Write error
+            System.out.println("JsonIO: saveJSONFile: Something went wrong when saving, error message: \"" + e.getMessage() + "\"");
+            throw e;
+        }
+    }
+
+    /**
+     * Public function readJSONFile
+     * Reads from a .json-file
+     *
+     * @param fileName      the directory of the file
+     * @return JSONObject   the loaded object
+     * @throws Exception    when something goes wrong with loading
+     */
+    public static JSONObject readJSONFile(String fileName) throws Exception {
+        JSONObject JSONNautilusObject = null;
+        try {
+            //Check Extension
+            String extension;
+            int i = fileName.lastIndexOf('.');
+            if (i > 0) {
+                extension = fileName.substring(i + 1);
+            } else {
+                throw new Exception("This is not a file.");
+            }
+            if (!extension.equals("json")) {
+                throw new Exception("The file is not a JSON file.");
+            } else {
+                //Read from file
+                FileReader file = new FileReader(fileName);
+                JSONParser parser = new JSONParser();
+                JSONNautilusObject = (JSONObject) parser.parse(file);
+            }
+        } catch (Exception e) {
+            //Read error
+            System.out.println("JsonIO: readJSONFile: Something went wrong when loading, error message: \"" + e.getMessage() + "\"");
+            throw e;
+        }
+        return JSONNautilusObject;
+    }
 }
--- HighFid/Model/Model.java
+++ HighFid/Model/Model.java
@@ -1,4 +1,90 @@
 package HighFid.Model;
 
+//Personal Imports
+import HighFid.Model.FileIO.JsonIO;
+import org.json.simple.JSONObject;
+
+/**
+ * Class Model
+ * Represents the dummy data of the application
+ *
+ * @author Tim Mesotten
+ */
 public class Model {
+
+    //Private members
+    private String name;
+
+    /**
+     * Constructor
+     * Creates a new Model
+     */
+    Model() {
+        this.setName("User-Centered Software Engineering");
+    }
+
+    //Getters & Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Public functions
+    /**
+     * Public function toJSON
+     * Method that converts Model to JSONObject
+     *
+     * @param fileName the name of the file
+     * @return true if successful write, false otherwise
+     */
+    @SuppressWarnings("unchecked")
+    public boolean toJSON(String fileName) {
+        try{
+            JSONObject JSONModel = new JSONObject();
+            JSONModel.put("name", this.getName());
+            JsonIO.saveJSONFile(fileName, JSONModel);
+            return true;
+        }catch(Exception e){
+            System.out.println("Model: toJSON: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * Public static function checkJSON
+     * Method that checks if Object has all the fields
+     *
+     * @param JSONModel JSONObject that has to be checked
+     * @return true if JSONModel has required fields, false otherwise
+     */
+    public static boolean checkJSON(JSONObject JSONModel) {
+        return JSONModel.get("name") != null;
+        //Check deeper classes via CLASS.checkJSON(JSONModel.get("what you need"))
+    }
+
+    /**
+     * Private function fromJSON
+     * Method that converts JSONObject to Model-object
+     *
+     * @param fileName The name of the file
+     * @return True if successful read, false otherwise
+     */
+    public boolean fromJSON(String fileName) {
+        JSONObject JSONModel;
+        try{
+            JSONModel = JsonIO.readJSONFile(fileName);
+            if (Model.checkJSON(JSONModel)) {
+                this.setName((String) JSONModel.get("name"));
+                return true;
+            } else {
+                return false;
+            }
+        } catch(Exception e){
+            System.out.println("Model: romJSON: " + e.getMessage());
+            return false;
+        }
+    }
 }
